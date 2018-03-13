6a9c1e9704c36993d643302d3f573312b791e4a4
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-13 19:33:53 +0700

    exercise 3 cari median

---
+++ exercise-3carimedian.js
@@ -0,0 +1,18 @@
+function cariMedian(arr) {
+  var middle = Math.floor((arr.length-1)/2);
+  
+  for (i=0; i < arr.length; i++) {
+    if (arr.length % 2 !== 0) {
+      return arr[middle];
+    } else if (arr.length % 2 === 0) {
+      return (arr[middle] + arr[middle + 1]) /2;
+    }
+  }
+}
+
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
+console.log(cariMedian([2, 2, 3, 3]));
