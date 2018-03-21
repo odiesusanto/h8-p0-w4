a715f5f9ba50064f40f7b10f2112cffbe169d8df
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-21 11:59:03 +0700

    edit return false for after looping

--- exercise-10checkAB.js
+++ exercise-10checkAB.js
@@ -4,11 +4,10 @@ function checkAB(str) {
         if(str[i] === 'a' || str[i] === 'b') {
             if (str[i+4] == 'a' || str[i+4] == 'b') {
                 return true;
-            } else {
-                return false;
             }
         }
     }
+    return false; //put the false here because have to loop the whole string first
 }
 console.log(checkAB('lane borrowed')); // true
 console.log(checkAB('i am sick')); // false
