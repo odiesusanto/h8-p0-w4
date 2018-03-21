fe516863725671790bbe3fc9586a72e1948615fa
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-21 11:52:56 +0700

    exercise 10 check AB

---
+++ exercise-10checkAB.js
@@ -0,0 +1,17 @@
+function checkAB(str) {
+    
+    for (var i=0; i < str.length; i++) {
+        if(str[i] === 'a' || str[i] === 'b') {
+            if (str[i+4] == 'a' || str[i+4] == 'b') {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
+console.log(checkAB('lane borrowed')); // true
+console.log(checkAB('i am sick')); // false
+console.log(checkAB('you are boring')); // true
+console.log(checkAB('barbarian')); // true
+console.log(checkAB('bacon and meat')); // false
