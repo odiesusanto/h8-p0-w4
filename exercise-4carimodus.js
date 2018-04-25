097e4a46d64596287f76223054b471623b3795e3
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-04-25 15:05:43 +0700

    edit exercise 4 cari modus

--- exercise-4carimodus.js
+++ exercise-4carimodus.js
@@ -4,7 +4,7 @@ function cariModus (arr) {
 
     for (var i=0; i < arr.length; i++) {
         var number = 0;
-        for (var j=1+i; j < arr.length; j++) {
+        for (var j=i+1; j < arr.length; j++) {
             if (arr[i] === arr[j]) {
                 number++;
             }
