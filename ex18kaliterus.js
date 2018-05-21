67c76c45869b9551770dca0c5502387f8362bb47
Odelia <odieawesome@DEP.localdomain>
2018-05-21 17:52:12 +0700

    add exercise 18 kali terus

---
+++ ex18kaliterus.js
@@ -0,0 +1,24 @@
+function kaliTerusRekursif(angka) {
+    
+    if (angka < 10) {
+        return angka;
+    } else {
+        var strAngka = String(angka);
+        if (strAngka.length > 1) { 
+            var count = 1;
+            for (var i = 0; i < strAngka.length; i++) {
+                count *= strAngka[i];
+            }
+            strAngka = count.toString();
+            return kaliTerusRekursif(Number(strAngka));
+        }
+    }
+}
+
+
+// TEST CASES
+console.log(kaliTerusRekursif(66)); // 8
+console.log(kaliTerusRekursif(3)); // 3
+console.log(kaliTerusRekursif(24)); // 8
+console.log(kaliTerusRekursif(654)); // 0
+console.log(kaliTerusRekursif(1231)); // 6
